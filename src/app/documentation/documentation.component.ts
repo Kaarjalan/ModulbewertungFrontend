b7749855fa81a392d6cf45af10cfc73ceae01d57
Karelier <arseny.zakharov@tn.mischok.academy>
2024-07-04 15:11:56 +0200

    wip

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [],
+  imports: [UserComponent],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.css'
 })
 export class DocumentationComponent {
-
+  
 }
