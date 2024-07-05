0fe6f90ce1fbd5b45f45d352d23959a466689b24
Karelier <arseny.zakharov@tn.mischok.academy>
2024-07-05 10:46:11 +0200

    wip Bug nicht behoben

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [UserComponent],
+  imports: [],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.css'
 })
