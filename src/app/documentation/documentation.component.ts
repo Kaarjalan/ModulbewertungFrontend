5b9803bd18d8d55a9e1dbe8d8e047beae83d6445
Karelier <arseny.zakharov@tn.mischok.academy>
2024-07-08 13:56:14 +0200

    Eingabe für Abschnitt 1 funktioniert

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { DVollstaendigComponent } from './d-vollstaendig/d-vollstaendig.component';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
+import { Abschnitt } from '../bewertung.models/bewertung.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [DVollstaendigComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.css'
 })
 export class DocumentationComponent {
-  
+  abschnitte: Abschnitt[] = [
+    {
+    nummer: 1, 
+    name: "Ist die Dokumentation formal vollständig?",
+    faktor: 0.5,
+    bewertungsKriterien: [
+      { name: "Seitenangaben",
+        punkte: 0,
+        notiz: "etwas"
+      },
+      {name: "Quellenangaben",
+        punkte: 0, 
+        notiz: "etwas"
+      },
+      {name: "Inhaltsverzeichnis/Gliederung",
+        punkte: 0, 
+        notiz: "etwas"
+      },
+      {name: "Zeitplan",
+        punkte: 0, 
+        notiz: "etwas"
+      },
+      {name: "Anlagenverzeichnis",
+        punkte: 0, 
+        notiz: "etwas"
+      },],
+      notiz: "Input Dozent"
+    },
+  ];
+  gesamtErgebnis: number = 0;
+  getAbschnittPunkte(abschnitt: Abschnitt):number {
+    return abschnitt.bewertungsKriterien.reduce((sum, kriterium) => sum + kriterium.punkte, 0)
+  }
+  getAbschnittErgebnis(abschnitt: Abschnitt): number {
+    return this.getAbschnittPunkte(abschnitt) * abschnitt.faktor;
+  }
+  updateGesamtErgebnis(){
+
+  }
 }
