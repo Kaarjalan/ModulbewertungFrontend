de4b12bad8d46c5ef760686524e9e2f33a2dc9c1
Karelier <arseny.zakharov@tn.mischok.academy>
2024-07-08 10:51:15 +0200

    Erste Komponente separiert + globales Styling eingeführt (orange)

---
+++ src/app/documentation/d-vollstaendig/d-vollstaendig.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d-vollstaendig',
+  standalone: true,
+  imports: [],
+  templateUrl: './d-vollstaendig.component.html',
+  styleUrl: './d-vollstaendig.component.css'
+})
+export class DVollstaendigComponent {
+
+}
--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DVollstaendigComponent } from './d-vollstaendig/d-vollstaendig.component';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [],
+  imports: [DVollstaendigComponent],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.css'
 })
