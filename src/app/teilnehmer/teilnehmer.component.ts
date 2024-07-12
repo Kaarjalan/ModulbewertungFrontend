9b3a8aa08479c35e4b888aecf20086a7ea23d721
Karelier <arseny.zakharov@tn.mischok.academy>
2024-07-12 14:13:43 +0200

    minor Bugfix

--- src/app/teilnehmer/teilnehmer.component.ts
+++ src/app/teilnehmer/teilnehmer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-teilnehemr',
+  selector: 'app-teilnehmer',
   standalone: true,
   imports: [],
   templateUrl: './teilnehmer.component.html',
